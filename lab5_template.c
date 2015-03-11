26066a1a6314211e157adbaf9f89a2403335ef7d
Justin Stewart <scubastew@csu.fullerton.edu>
2015-03-10 17:07:50 -0700

    First commit, project works except on large data."

---
+++ lab5_template.c
@@ -0,0 +1,202 @@
+/*
+  CPSC 223C
+  Spring 2015
+  CSU Fullerton
+
+  Lab 5: word frequencies
+
+  Authors: Justin Stewart (scubastew@csu.fullerton.edu)
+
+*/ 
+
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NOT_FOUND -1
+
+/* Safe estimate of the length of the longest English word. */
+#define MAX_WORD_LENGTH 200
+
+/* Read the contents of the text file path, count how many times each
+   whitespace-separated word appears, then for each distinct word, print one line
+
+   <WORD> <COUNT>
+
+   to standard output, where <WORD> is the non-whitespace characters
+   of the word, and <COUNT> is the number of times it appears, which
+   must be a positive integer.
+
+   Return true on success, or false on any kind of error. */
+bool print_word_frequencies(char* path);
+
+/* Opaque data type for a data structure that tracks the number of
+   occurrences of strings. */
+struct WordCounts {
+  struct Node* head;
+  struct Node* tail;
+  int n;
+};
+
+struct Node {
+  int count;
+  char *data;
+  struct Node *next;
+  struct Node *prev;
+};
+
+/* Allocate a new word count object, or return NULL on memory
+   error. */
+struct WordCounts* word_counts_make();
+
+/* Free a WordCounts object. */
+void word_counts_free(struct WordCounts* wc);
+
+/* If word is already in wc, increment its count by 1. If word is not
+   present, add it with a count of 1. Return true on success and false
+   on memory error. */
+bool word_counts_increment(struct WordCounts* wc, char* word);
+
+/* If word is in wc, return its frequency count. If word is not in wc,
+   return NOT_FOUND. */
+int word_counts_lookup(struct WordCounts* wc, char* word);
+
+/* Print out all the words and counts in wc, using the same format as
+   print_word_frequences. */
+void word_counts_print(struct WordCounts* wc);
+
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    fputs("error: you must supply a filename as an argument\n", stderr);
+    return 1;
+  }
+
+  if (!print_word_frequencies(argv[1]))
+    return 1;
+
+  return 0;
+}
+
+bool print_word_frequencies(char* path) {
+  /* TODO: finish this function definition */
+
+  FILE* f;
+  struct WordCounts* wc;
+  char str[MAX_WORD_LENGTH + 1];
+
+  f = fopen(path, "r");
+  if (f == NULL) {
+    fprintf(stderr, "error: could not open '%s'", path);
+    return false;
+  }
+
+  wc = word_counts_make();
+  if (wc == NULL) {
+    fprintf(stderr, "error: out of memory");
+    fclose(f);
+    return false;
+  }
+
+  fprintf(stderr, "processing '%s'...", path);
+
+  while (fscanf(f,"%s", str) != EOF) {
+    if (!word_counts_increment(wc, str)) {
+      fprintf(stderr, "\nerror: out of memory");
+      fclose(f);
+      word_counts_free(wc);
+      return false;
+    }
+  }
+
+  fclose(f);
+
+  fprintf(stderr, "done\n");
+
+  word_counts_print(wc);
+  
+  word_counts_free(wc);
+
+  return true;
+}
+
+struct WordCounts* word_counts_make() {
+  struct WordCounts* it = malloc(sizeof(struct WordCounts));
+  if (it == NULL)
+    return NULL;
+  
+  it->n = 0;
+  it->head = NULL;
+  it->tail = NULL;
+
+  return it;
+}
+
+void word_counts_free(struct WordCounts* wc) {
+  free(wc);
+}
+
+int word_counts_lookup(struct WordCounts* wc, char* word) {
+  struct Node* p;
+  int bo;
+
+  bo = 0;
+
+  p = wc->head;
+
+  if(wc->head != NULL) {
+    while(bo == 0 && p != NULL) {
+      if(strcmp(p->data, word) == 0) {
+	p->count++;
+	bo = 1;
+      }
+      p = p->next;
+    }
+  }
+
+  return bo;
+}
+
+bool word_counts_increment(struct WordCounts* wc, char* word) {
+  struct Node* p;
+  struct Node* q;
+  
+  if(!word_counts_lookup(wc, word)) {
+    q = wc->tail;
+    p = malloc(sizeof(struct Node));
+    p->data = malloc(sizeof(word));
+    strcpy(p->data, word);
+    p->count = 1;
+    p->next = NULL;
+
+    if(wc->head == NULL) {
+      p->prev = wc->head;
+      wc->head = p;
+      wc->tail = p;
+      wc->n++;
+    }
+    else {
+      q->next = p;
+      p->prev = q;
+      wc->tail = p;
+      wc->n++;
+    }
+  }
+
+  return true;
+      
+}
+
+void word_counts_print(struct WordCounts* wc) {
+  struct Node* p;
+
+  p = wc->head;
+
+  while(p != NULL) {
+    printf("%s %d\n", p->data, p->count);
+    p = p->next;
+  }
+
+  printf("\n");
+}
